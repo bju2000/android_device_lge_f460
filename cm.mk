7d3ff21855f18765e8bce45723a8c2b1188cc4f1
Shawn Hwei <meetshawn@users.noreply.github.com>
2015-11-24 17:21:35 +0800

    Update cm.mk

--- cm.mk
+++ cm.mk
@@ -1,4 +1,4 @@
-$(call inherit-product, device/lge/d855/full_d855.mk)
+$(call inherit-product, device/lge/f460/full_f460.mk)
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
@@ -6,7 +6,7 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 # Enhanced NFC
 $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
-PRODUCT_NAME := cm_d855
+PRODUCT_NAME := cm_f460
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE="g3" \
