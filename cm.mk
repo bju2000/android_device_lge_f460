da4f23dabd5abaf664cbc92882b6f0a4f51b6404
Shawn Hwei <meetshawn@users.noreply.github.com>
2015-11-25 16:41:04 +0800

    Update cm.mk

--- cm.mk
+++ cm.mk
@@ -9,7 +9,7 @@ $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 PRODUCT_NAME := cm_f460
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_DEVICE="g3" \
-    PRODUCT_NAME="g3_global_com" \
-    BUILD_FINGERPRINT="lge/g3_global_com/g3:5.0/LRX21R.A1419207951/1419207951:user/release-keys" \
-    PRIVATE_BUILD_DESC="g3_global_com-user 5.0 LRX21R.A1419207951 1419207951 release-keys"
+    PRODUCT_DEVICE="tiger6" \
+    PRODUCT_NAME="tiger6_skt_kr" \
+    BUILD_FINGERPRINT="lge/tiger6_skt_kr/tiger6:5.0.1/LRX21Y/F460S20m.1439790356:user/release-keys" \
+    PRIVATE_BUILD_DESC="tiger6_skt_kr-user 5.0.1 LRX21Y F460S20m.1439790356 release-keys"
